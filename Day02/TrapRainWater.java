6c5c2c3a857f13431f62279ca5da20873e76e833
papAdmin-code <prixen.personal@gmail.com>
2025-01-29 19:22:37 +0530

    O(n)

--- Day02/TrapRainWater.java
+++ Day02/TrapRainWater.java
@@ -1,22 +1,33 @@
 import java.util.*;
 
 class Main {
-    public static void main(String[] args) {
-        int[] arr = {2, 1, 5, 3, 1, 0, 4};
-        int res = 0;
+    public static int trapRainwater(int[] arr) {
+        int n = arr.length;
+        if (n == 0) return 0;
 
-        for (int i = 1; i < arr.length - 1; i++) {
-            int left = arr[i];
-            for (int j = 0; j < i; j++)
-                left = Math.max(left, arr[j]);
+        int[] leftMax = new int[n];
+        int[] rightMax = new int[n];
 
-            int right = arr[i];
-            for (int j = i + 1; j < arr.length; j++)
-                right = Math.max(right, arr[j]);
-
-            res += Math.min(left, right) - arr[i];
+        leftMax[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], arr[i]);
         }
 
-        System.out.println(res);
+        rightMax[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], arr[i]);
+        }
+
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            res += Math.min(leftMax[i], rightMax[i]) - arr[i];
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 1, 5, 3, 1, 0, 4};
+        System.out.println(trapRainwater(arr));
     }
 }
