f92b958f2ae96911bb85d9dd7e07a9423d997760
YeswanthVelpuru <169123734+YeswanthVelpuru@users.noreply.github.com>
2025-02-01 16:37:17 +0530

    Update ThreeSum.java

--- Day02/ThreeSum.java
+++ Day02/ThreeSum.java
@@ -1,10 +1,14 @@
+import java.util.*;
+
 class Solution {
     public List<List<Integer>> threeSum(int[] arr) {
         Arrays.sort(arr);
         int n = arr.length;
         List<List<Integer>> ans = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            if (arr[i] > 0) break; 
+            if (arr[i] > 0) break;
+            if (i > 0 && arr[i] == arr[i - 1]) continue; // Skip duplicate elements for 'i'
+
             int l = i + 1, r = n - 1;
             while (l < r) {
                 int sum = arr[i] + arr[l] + arr[r];
@@ -12,13 +16,35 @@ class Solution {
                 else if (sum > 0) r--;
                 else {
                     ans.add(Arrays.asList(arr[i], arr[l], arr[r]));
-                    while (l+1 <= r && arr[l] == arr[l+1]) l++;
+
+                    while (l < r && arr[l] == arr[l + 1]) l++; // Skip duplicate elements for 'l'
+                    while (l < r && arr[r] == arr[r-1]) r--; // Skip duplicate elements for 'r'
+                    
                     l++;
                     r--;
                 }
             }
-            while (i+1 < n && arr[i+1] == arr[i]) i++; 
         }
         return ans;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] testCases = {
+                {-1, 0, 1, 2, -1, -4},
+                {0, 1, 1, 1},
+                {0, 0, 0, 0},
+                {-2, 0, 1, 1, 2},
+                {-1, -1, -1, 2, 2}
+        };
+
+
+        for (int[] arr : testCases) {
+            List<List<Integer>> result = solution.threeSum(arr);
+            System.out.println("Input: " + Arrays.toString(arr) + ", Output: " + result);
+        }
+
     }
 }
