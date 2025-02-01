b47f8e172c5551fef1e3c5ecc322ff097ef52c44
YeswanthVelpuru <169123734+YeswanthVelpuru@users.noreply.github.com>
2025-02-01 16:41:02 +0530

    Update addMiddle.java

--- Day03/addMiddle.java
+++ Day03/addMiddle.java
@@ -9,12 +9,10 @@ class Node {
 }
 
 public class Main {
-    
-    static Node insertAfter(Node head, int key,
-                                   int newData) {
+
+    static Node insertAfter(Node head, int key, int newData) {
         Node curr = head;
-        
-        
+
         while (curr != null) {
             if (curr.data == key)
                 break;
@@ -38,16 +36,25 @@ public class Main {
     }
 
     public static void main(String[] args) {
-      
         Node head = new Node(2);
         head.next = new Node(3);
         head.next.next = new Node(5);
         head.next.next.next = new Node(6);
 
-        int key = 3, newData = 4;
+        int key = 3;
+        int newData = 4;
+
+        System.out.print("Original list: ");
+        printList(head);
+
         head = insertAfter(head, key, newData);
 
-
+        System.out.print("Modified list: ");
         printList(head);
+
+        // Test with key that is not in the list:
+         System.out.print("List after trying to insert after key 7 (not in the list): ");
+         head=insertAfter(head,7,8);
+         printList(head);
     }
 }
